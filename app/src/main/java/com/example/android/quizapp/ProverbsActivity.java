10a00abbcdec3857ce0020dbcb01ae9129264c24
flisiq <flisiq@gmail.com>
2017-04-11 20:27:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/quizapp/ProverbsActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ProverbsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proverbs);
+    }
+
+}
